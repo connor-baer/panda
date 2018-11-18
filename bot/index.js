4f272a4f150190f0b6c226ac3f690c1ffa1a892e
Connor Bär <connor.baer@sumup.com>
2018-11-18 13:50:41 +0100

    Add updater logger

--- bot/index.js
+++ bot/index.js
@@ -1,17 +1,12 @@
 const Telegraf = require('telegraf');
+const updateLogger = require('telegraf-update-logger');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+bot.use(updateLogger({ colors: true }));
+
 bot.telegram.getMe().then(botInfo => {
   bot.options.username = botInfo.username;
-});
-
-bot.use((ctx, next) => {
-  const start = new Date();
-  return next(ctx).then(() => {
-    const ms = new Date() - start;
-    console.log('Response time %sms', ms);
-  });
 });
 
 bot.hears(/hello/i, ctx => ctx.reply('Hello world'));
--- routes/bot.js
+++ routes/bot.js
@@ -1,9 +1,12 @@
+const { json } = require('micro');
+
 const bot = require('../bot');
 
 bot.telegram.setWebhook(
   `${process.env.NOW_URL}/bot/${process.env.WEBHOOK_SECRET}`
 );
 
-module.exports = (req, res) => {
-  bot.handleUpdate(req.body, res);
+module.exports = async (req, res) => {
+  const body = await json(req);
+  bot.handleUpdate(body, res);
 };
